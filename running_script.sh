73bf014f64ec1d314301334d075de0440e1c4f4a
JKS_CPUcom <jajajang@kaist.ac.kr>
2021-06-10 05:08:50 +0900

    fix running_script.sh

--- running_script.sh
+++ running_script.sh
@@ -1,4 +1,4 @@
 for A in  bloful bmoracle bltwostage-bm-sp_simple2 bltwostage-sp_simple2  
 do
-python3  run_expr01.py ${A} -d 8 -R 0.01 -r 1 -T 10000 -tg 1 --nTry=60 |& tee log_0207_${A}.txt;
+python3  210425_run_expr01.py ${A} -d 8 -R 0.01 -r 1 -T 10000 -tg 1 --nTry=60 |& tee log_0207_${A}.txt;
 done
